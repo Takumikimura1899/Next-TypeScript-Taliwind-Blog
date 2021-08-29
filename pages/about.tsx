d2178fb489f539307a87b52f23f6abf09341ec05
Takumikimura141899 <kimura141899@gmail.com>
2021-08-30 06:16:52 +0900

    header footer

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/layout';
+
+const About = () => {
+  return (
+    <Layout>
+      <div className="text-center mt-10">about</div>
+    </Layout>
+  );
+};
+
+export default About;
