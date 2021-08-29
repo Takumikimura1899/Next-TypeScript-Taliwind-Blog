8b217104d4efec77e12c2bffba44690434cef736
Takumikimura141899 <kimura141899@gmail.com>
2021-08-29 17:00:33 +0900

    apiget

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -19,7 +19,7 @@ export default function Post({ postData }: any) {
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds();
+  const paths = await getAllPostIds();
   return {
     paths,
     fallback: false,
